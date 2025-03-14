9a4fa9bb4d8552f8c8032eef3797ab003fa73498
Juan Valdes <57189220+C295643@users.noreply.github.com>
2025-03-14 02:02:44 -0400

    Updadte constants and tsconfig

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const PARENT_BACKGROUND = { backgroundColor: "lightgray" }; // #D3D3D3
+export const MAIN_CONTAINER_BORDER = { border: "dashed 4px red" };
+export const CONTAINER_BORDER = { border: "dashed 4px blue" };
+export const ITEM_BORDER = { border: "dashed 4px fuchsia" };

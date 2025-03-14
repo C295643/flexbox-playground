6c45c6a9a18ed56142685645afc9ad3acf481403
Juan Valdes <57189220+C295643@users.noreply.github.com>
2025-03-14 02:24:37 -0400

    Upadte Container types file leaving an enum emulator

--- src/types/Container.ts
+++ src/types/Container.ts
@@ -1,17 +1,9 @@
 import { ObjectValues } from "./types";
 
 export const ContainerType = {
-  MAIN: 'MAIN',
-  CONTAINER: 'CONTAINER',
-  ITEM: 'ITEM',
+  MAIN: "MAIN",
+  CONTAINER: "CONTAINER",
+  ITEM: "ITEM",
 } as const;
 
 export type ContainerType = ObjectValues<typeof ContainerType>;
-
-export const ContainerResetClass = {
-  MAIN: 'container-main',
-  CONTAINER: 'container',
-  ITEM: 'item',
-} as const;
-
-export type ContainerResetClass = ObjectValues<typeof ContainerResetClass>;

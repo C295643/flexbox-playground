8b897c7708e0b070cdc184005ce758a5aaef6977
Juan Valdes <57189220+C295643@users.noreply.github.com>
2025-03-21 02:43:19 -0400

    Install bootstrap-icons dependency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'; // Import Bootstrap Icons
 import './globals.css';
 import type { Metadata } from 'next';
 
